0e0abc4d7adf90046414dfec251d1094a69f69f5
Sachin Kumar <sachinkmr375@gmail.com>
2016-03-24 19:27:15 +0530

    Added App bar

--- app/src/main/java/com/goal/sachin/goalassistant/MainActivity.java
+++ app/src/main/java/com/goal/sachin/goalassistant/MainActivity.java
@@ -2,12 +2,18 @@ package com.goal.sachin.goalassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
     }
 }
