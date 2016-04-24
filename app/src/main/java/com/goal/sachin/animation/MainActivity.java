150f3f9af331cc7aa6441d8d16f2d372387d5352
Sachin Kumar <sachinkmr375@gmail.com>
2016-04-24 08:30:52 +0530

    pushing animation project

---
+++ app/src/main/java/com/goal/sachin/animation/MainActivity.java
@@ -0,0 +1,34 @@
+package com.goal.sachin.animation;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    AnimationDrawable rocketAnimation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.image);
+        imageView.setBackgroundResource(R.drawable.rocket_thrust);
+
+        rocketAnimation = (AnimationDrawable) imageView.getBackground();
+
+        rocketAnimation.start();
+
+
+
+    }
+
+}
